dd5074074c0cd524453db7bb96862dfb07d3cd4f
berrios96sean <berrios96sean@gmail.com>
2024-02-10 13:00:45 -0700

    setting up aliases for vitis/vivado

---
+++ utils/l_vitis.sh
@@ -0,0 +1,6 @@
+source /scratch/kmhatre/AMD_tools/Vitis_HLS/2022.1/settings64.sh
+/scratch/kmhatre/AMD_tools/Vitis_HLS/2022.1/bin/vitis_hls
+
+
+
+
---
+++ utils/l_vivado.sh
@@ -0,0 +1,7 @@
+source /scratch/kmhatre/AMD_tools/Vivado/2022.1/settings.64.sh
+/scratch/kmhatre/AMD_tools/Vivado/2022.1/bin/vivado 
+
+
+
+
+
