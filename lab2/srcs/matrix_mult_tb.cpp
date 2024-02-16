72516f2139a2f6b2c2e3b978d7b0e4361c5fb9cb
Sean Berrios <berrios96sean@gmail.com>
2024-02-15 23:38:28 -0700

    adding in random test -- may not be best approach

--- lab2/srcs/matrix_mult_tb.cpp
+++ lab2/srcs/matrix_mult_tb.cpp
@@ -1,6 +1,7 @@
 #include "matrix_mult.h"
 #include <iostream>
 #include <iomanip>
+#include <random>
 // Matrix Mult Libraries for verification from boost
 #include <boost/numeric/ublas/matrix.hpp>
 #include <boost/numeric/ublas/io.hpp>
@@ -10,17 +11,36 @@ void print_matrix(mat_prod matrix[IN_A_ROWS][IN_B_COLS])
 {
     for(int i = 0; i < IN_A_ROWS; ++i) {
         for(int j = 0; j < IN_B_COLS; ++j) {
-            std::cout << std::setw(8) << matrix[i][j] << " ";
+            std::cout << std::fixed << std::setprecision(1) << std::setw(8) << matrix[i][j] << " ";
         }
         std::cout << "\n";
     }
 }
 
+void convert_matrix(const boost::numeric::ublas::matrix<double>& input_matrix, mat_a output_array[IN_A_ROWS][IN_A_COLS]) {
+    for (int i = 0; i < std::min(IN_A_ROWS, (int)input_matrix.size1()); ++i) {
+        for (int j = 0; j < std::min(IN_A_COLS, (int)input_matrix.size2()); ++j) {
+            output_array[i][j] = static_cast<mat_a>(input_matrix(i, j));
+        }
+    }
+}
+
 int main()
 {
+    srand(static_cast<unsigned int>(time(0))); // random seed 
+
+    // Random Matrix test 
+    boost::numeric::ublas::matrix<int> rand_a(IN_A_ROWS,IN_A_COLS);
+    boost::numeric::ublas::matrix<int> rand_b(IN_B_ROWS,IN_B_COLS);
+    mat_a rand_a2[IN_A_ROWS][IN_A_COLS]; 
+    mat_b rand_b2[IN_A_ROWS][IN_A_COLS]; 
+    mat_prod rand_prod[IN_A_ROWS][IN_A_COLS];
+
+    // Matrix filled with Ones
     mat_a a[IN_A_ROWS][IN_A_COLS]; 
     mat_b b[IN_A_ROWS][IN_A_COLS]; 
     mat_prod prod[IN_A_ROWS][IN_A_COLS];
+    mat_prod rand_prod2[IN_A_ROWS][IN_A_COLS];
     mat_prod prod_expected[IN_A_ROWS][IN_A_COLS];
 
     // fill with all ones 
@@ -32,7 +52,25 @@ int main()
         }
     }
 
+    // fill with random values 
+    for (int i = 0; i < IN_A_ROWS; ++i) {
+        for (int j = 0; j < IN_A_COLS; ++j) {
+            rand_a(i, j) = static_cast<int>(rand()) / (RAND_MAX / 9.0); // fill with # 0-9
+            rand_b(i, j) = static_cast<int>(rand()) / (RAND_MAX / 9.0); // fill with # 0-9
+        }
+    }
+
+    convert_matrix(rand_a,rand_a2);
+    convert_matrix(rand_b,rand_b2);
+    // print_matrix(rand_a2);
+    // print_matrix(rand_b2);
+
     matrix_mult(a,b,prod);
+    matrix_mult(rand_a2,rand_b2,rand_prod);
+
+    boost::numeric::ublas::matrix<double> rand_prod_expected = boost::numeric::ublas::prod(rand_a,rand_b);
+
+    convert_matrix(rand_prod_expected,rand_prod2);
 
     // print_matrix(prod);
     // print_matrix(prod_expected);
@@ -49,10 +87,26 @@ int main()
         }
     }
 
+    bool rand_passed = true;  
+    for (int i = 0; i < IN_A_ROWS; ++i) {
+        for (int j = 0; j < IN_A_COLS; ++j) {
+            if (rand_prod[i][j] - rand_prod2[i][j] != 0) // if outputs dont match
+            {
+                rand_passed = false;
+            }
+        }
+    }
+
     if (passed) {
-        std::cout << "TEST PASSED\n";
+        std::cout << "BASIC TEST PASSED\n";
     } else {
-        std::cout << "TEST FAILED\n";
+        std::cout << "BASIC TEST FAILED\n";
+    }
+
+    if (rand_passed) {
+        std::cout << "RANDOM TEST PASSED\n";
+    } else {
+        std::cout << "RANDOM TEST FAILED\n";
     }
 
     return 0;
